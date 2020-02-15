675d13be82fb94f5eb655429f1c826256856254b
Computer <Computer@DESKTOP-E61LJDK>
2020-02-15 22:09:04 +0100

    Add project files.

---
+++ kolac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace kolac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite broj:");
+            double x = double.Parse(Console.ReadLine());
+            while (x > 1)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x /= 2);
+                }
+                else if (x % 2 != 0)
+                {
+                    Console.WriteLine(x = x * 3 + 1);
+                    
+                }
+            }
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
